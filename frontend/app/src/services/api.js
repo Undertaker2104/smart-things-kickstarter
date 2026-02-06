2033eb288da90a02033a0f4d18f0a3d2771fdf31
Undertaker <124693454+Undertaker2104@users.noreply.github.com>
2026-02-06 12:09:22 +0100

    Remove commented-out local API base URL from api.js

--- frontend/app/src/services/api.js
+++ frontend/app/src/services/api.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL = "http://145.24.237.126:8000";
-
+//export const API_BASE_URL = "http://localhost:8000" voor lokaal testen
 export const chartAPI = {
     getExpectedVsCounted: async () => {
         const response = await fetch(`${API_BASE_URL}/api/charts/expected-vs-counted`)
